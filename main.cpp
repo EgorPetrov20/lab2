977a1b95be4eb2618de77a1c793c2ec1664af294
EgorPetrov20 <egorpetrov882@gmail.com>
2024-11-22 12:01:52 +0500

    Реализованы вторая и третья функции

--- main.cpp
+++ main.cpp
@@ -9,20 +9,25 @@ void readLines(){
     
 }
 
-void showLines(){
-
-
-    
+void showLines(const vector<string>& vec){
+for (const auto& str : vec) {
+    cout << str << endl;   
 }
 
 void linesOut(){
-
-    
+    ofstream out2("2.txt");
+    string str1 = out1.str();
+    out2.write(str1.c_str(), str.size());
+    out2.close();
+    return 0;  
 }
 
 
 int main()
 {
     cout << "Hello world!" << endl;
+    readLines();
+    showLines();
+    linesOut();    
     return 0;
 }
